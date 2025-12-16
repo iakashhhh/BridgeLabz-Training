b558036d0526035ebfd406e12799a96f2ff1fffc
Akash Sharma <i.akashhhh@gmail.com>
2025-12-17 01:06:00 +0530

    [Akash] : Program to convert Kilometers to Miles

---
+++ gcr-codebase/program-prerequisite/KmToMiles.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class KmToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter distance in kilometers: ");
+        double km = sc.nextDouble();
+
+        double miles = km * 0.621371;
+        System.out.println("Distance in miles = " + miles);
+    }
+}
