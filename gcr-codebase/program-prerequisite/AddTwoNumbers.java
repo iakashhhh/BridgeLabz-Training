dc9b90c6c5329592ddf3b63ba1e3052f3887ced8
Akash Sharma <i.akashhhh@gmail.com>
2025-12-16 16:59:38 +0530

    [Akash] : added sum program

---
+++ gcr-codebase/program-prerequisite/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AddTwoNumbers{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First num : ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter Decond num : ");
+		int num2 = sc.nextInt();
+		System.out.println("Result : " + (num1+num2));
+	}
+}
