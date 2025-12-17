ddce348f23b4571626f4417ec9a19c217fe40b1e
Akash Sharma <i.akashhhh@gmail.com>
2025-12-18 01:30:16 +0530

    [Akash] : program to compute volume of earth

--- gcr-codebase/programming-elements/level1/KmtoMiles.java
+++ gcr-codebase/programming-elements/level1/KmtoMiles.java
@@ -1,7 +1,7 @@
 public class KmtoMiles{
 	public static void main(String[] args){
 	double km = 10.8;
-	double miles = km * 1.6;
+	double miles = km * 0.6;
 
 	System.out.println(" The distance " + km + " km in miles is " + miles);
 	}
