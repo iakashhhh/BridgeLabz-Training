8997b3e7d9772fbd2e29689dac1b1752c7e971aa
Akash Sharma <i.akashhhh@gmail.com>
2025-12-17 23:41:03 +0530

    [Akash] program to find current age

---
+++ gcr-codebase/programming-elements/level1/AgeOfHarry.java
@@ -0,0 +1,9 @@
+public class AgeOfHarry {
+	public static void main(String[] args){
+	int birthYear = 2000;
+	int currYear = 2024;
+	int age = currYear - birthYear;
+
+	System.out.println("Age of Harry in 2024 is " + age);
+	}
+}
