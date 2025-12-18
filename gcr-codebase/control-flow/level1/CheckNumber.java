3f861d99da4fa8b1e0444e1f7b2f3ac7b90e66e4
Akash Sharma <i.akashhhh@gmail.com>
2025-12-18 17:08:14 +0530

    [Akash] : Program to check number is positive or negative

---
+++ gcr-codebase/control-flow/level1/CheckNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class CheckNumber{
+	public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+
+	//taking number input from the user
+	int number = input.nextInt();
+
+	//check if num is positive or not
+	if(number > 0){
+		System.out.println("The number "+ number + " is positive");
+	}
+
+	//check if it is negative
+	else if(number < 0){
+		System.out.println("The number "+ number + " is negative");
+
+	}
+
+	//number is  zero
+	else{
+		System.out.println("The number is Zero");
+
+	}
+
+	}
+}
