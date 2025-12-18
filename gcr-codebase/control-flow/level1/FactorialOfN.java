580d561a8cffcb16e7643cc2d3ef56a507208024
Akash Sharma <i.akashhhh@gmail.com>
2025-12-19 00:01:08 +0530

    [Akash] : program to find factorial of number

---
+++ gcr-codebase/control-flow/level1/FactorialOfN.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class FactorialOfN{
+	public static void main(String[]args){
+            
+		//Create an instance of Scanner class to take user inputs
+		Scanner input = new Scanner(System.in);
+			
+		//Create a int variable named number and take input from user
+		System.out.println("Enter number: ");
+		int number = input.nextInt();
+		
+		int factorial = 0;
+		
+		if(number > 0){
+			factorial = 1;
+			while(number > 0){
+				factorial = factorial*number;
+				number--;
+			}
+		}
+		//printing result 
+		System.out.println("Factorial of number is " + factorial);
+		
+	    //Closing the scanner object
+	    input.close();
+	}
+}
