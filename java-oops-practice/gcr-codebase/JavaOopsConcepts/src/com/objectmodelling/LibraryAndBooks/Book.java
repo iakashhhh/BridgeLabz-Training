2408b77af76081472cb47bbbe87a6d85da9fe1e5
Akash Sharma <i.akashhhh@gmail.com>
2025-12-31 15:14:56 +0530

    [Akash] : Added Library Management program

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/LibraryAndBooks/Book.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/LibraryAndBooks/Book.java
@@ -1,5 +1,28 @@
 package com.objectmodelling.LibraryAndBooks;
+import java.util.*;
 
 public class Book {
+	
+    private String title;
+    private String author;
 
+    //Parameterized Constructor
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    //Getter for title
+    public String getTitle() {
+        return title;
+    }
+
+    //Getter for author
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void displayBook() {
+        System.out.println("Book Title: " + title + ", Author: " + author);
+    }
 }
