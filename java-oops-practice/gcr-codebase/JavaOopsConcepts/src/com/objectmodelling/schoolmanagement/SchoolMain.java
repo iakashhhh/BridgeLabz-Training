20979b24ee4ffa92aad1f0e760922f89b54d3c75
Akash Sharma <i.akashhhh@gmail.com>
2025-12-31 15:13:02 +0530

    [Akash] : Added school management program

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/schoolmanagement/SchoolMain.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/schoolmanagement/SchoolMain.java
@@ -2,4 +2,24 @@ package com.objectmodelling.schoolmanagement;
 
 public class SchoolMain {
 
+    public static void main(String[] args) {
+
+        School school = new School("ABC School");
+
+        Student s1 = new Student("Sanya");
+        Student s2 = new Student("Rahul");
+
+        Course java = new Course("Java");
+        Course dbms = new Course("DBMS");
+
+        school.addStudent(s1);
+        school.addStudent(s2);
+
+        s1.enrollCourse(java);
+        s1.enrollCourse(dbms);
+        s2.enrollCourse(java);
+
+        s1.viewCourses();
+        java.showEnrolledStudents();
+    }
 }
