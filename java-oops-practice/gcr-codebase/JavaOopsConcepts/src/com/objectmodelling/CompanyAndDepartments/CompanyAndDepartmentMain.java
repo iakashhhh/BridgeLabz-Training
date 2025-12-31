ca9d998c911345c058c2a6e48a9d43136f9affc6
Akash Sharma <i.akashhhh@gmail.com>
2025-12-31 15:18:36 +0530

    [Akash] : Added CompanyAndDepartments program

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/CompanyAndDepartments/CompanyAndDepartmentMain.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/CompanyAndDepartments/CompanyAndDepartmentMain.java
@@ -2,4 +2,25 @@ package com.objectmodelling.CompanyAndDepartments;
 
 public class CompanyAndDepartmentMain {
 
+	public static void main(String[] args) {
+		
+		Company company = new Company("TechCorp");
+
+        Department dev = new Department("Development");
+        dev.addEmployee("Alice");
+        dev.addEmployee("Bob");
+
+        Department hr = new Department("HR");
+        hr.addEmployee("Charlie");
+
+        company.addDepartment(dev);
+        company.addDepartment(hr);
+
+        company.showCompanyDetails();
+
+        // If company object is deleted, departments and employees are also deleted
+        company = null;
+
+	}
+
 }
--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/CompanyAndDepartments/Employee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/CompanyAndDepartments/Employee.java
@@ -1,5 +1,15 @@
 package com.objectmodelling.CompanyAndDepartments;
 
 public class Employee {
+	
+	private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public void showEmployee() {
+        System.out.println("Employee: " + name);
+    }
 
 }
