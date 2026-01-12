6f4d0936edf9c2ec11bfbcc397ee6753e8a18e54
Akash Sharma <i.akashhhh@gmail.com>
2026-01-13 00:43:56 +0530

    [Akash] : Added TrainCompanion – Compartment Navigation System

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/traincompanion/Compartment.java
@@ -0,0 +1,15 @@
+package traincompanion;
+
+public class Compartment {
+    String name;
+    String service; // Pantry, WiFi, Sleeper etc.
+    Compartment prev;
+    Compartment next;
+
+    public Compartment(String name, String service) {
+        this.name = name;
+        this.service = service;
+        this.prev = null;
+        this.next = null;
+    }
+}
