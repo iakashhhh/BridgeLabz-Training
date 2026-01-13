1b3e65862864b529222741956f6bb1a1fc76dc84
Akash Sharma <i.akashhhh@gmail.com>
2026-01-13 12:07:01 +0530

    [Akash] : TrafficManager – Roundabout Vehicle Flow

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/trafficmanager/Vehicle.java
@@ -0,0 +1,11 @@
+package trafficmanager;
+
+public class Vehicle {
+    String vehicleNumber;
+    Vehicle next;
+
+    public Vehicle(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+        this.next = null;
+    }
+}
