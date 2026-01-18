6562b8c1222276a7b0c8395010d10cc7a2e4dbb7
Akash Sharma <i.akashhhh@gmail.com>
2026-01-18 12:08:35 +0530

    [Akash] : Added ArtExpo – Sort Artists by Registration Time

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/artexpo/ArtistSorting.java
@@ -0,0 +1,27 @@
+package artexpo;
+
+public class ArtistSorting {
+	
+	public static void insertionSort(Artist[] artist, int n) {
+		for(int i=1; i<n; i++) {
+			Artist key = artist[i];
+			int j= i-1;
+			
+			while(j>=0 && artist[j].time.compareTo(key.time) > 0) {
+				artist[j+1] = artist[j];
+				j--;
+			}
+			
+			artist[j+1] = key;
+		}
+	}
+	
+	public static void display(Artist[] artist) {
+		for(Artist a : artist) {
+			System.out.println("Name : " + a.name);
+			System.out.println("Registration Time : " + a.time);
+			System.out.println();
+		}
+	}
+
+}
