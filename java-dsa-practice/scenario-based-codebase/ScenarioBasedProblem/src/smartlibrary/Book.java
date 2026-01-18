d4352c57bae9b8afc10ff346ac613e50a185faf7
Akash Sharma <i.akashhhh@gmail.com>
2026-01-18 15:55:34 +0530

    [Akash] : Added SmartLibrary – Auto-Sort Borrowed Books

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/smartlibrary/Book.java
@@ -0,0 +1,11 @@
+package smartlibrary;
+
+public class Book {
+	
+	String name;
+	
+	Book(String name){
+		this.name = name;
+	}
+
+}
---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/smartlibrary/BookSorting.java
@@ -0,0 +1,28 @@
+package smartlibrary;
+
+
+//import artexpo.Artist;
+
+public class BookSorting {
+	
+	public static void insertionSort(Book[] bookList, int n ) {
+		for(int i=1; i<n; i++) {
+			Book key = bookList[i];
+			int j = i-1;
+			
+			while(j>=0 && bookList[j].name.compareToIgnoreCase(key.name) > 0) {
+				bookList[j+1] = bookList[j];
+				j--;
+			}
+			
+			bookList[j+1] = key;
+	}
+	}
+	
+	public static void display(Book[] bookList) {
+		for(Book b : bookList) {
+			System.out.println( b.name + " ");
+		}
+	}
+
+}
