955b4ad36596b3718718269355e975e52c1f2015
Akash Sharma <i.akashhhh@gmail.com>
2026-01-18 15:49:30 +0530

    [Akash] : Added IceCreamRush – Sorting Flavors by Popularity

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/icecreamrush/IceCream.java
@@ -0,0 +1,13 @@
+package icecreamrush;
+
+public class IceCream {
+	
+	String name;
+	double rating;
+	
+	public IceCream(String name, double rating) {
+		this.name = name;
+		this.rating = rating;
+	}
+	
+}
