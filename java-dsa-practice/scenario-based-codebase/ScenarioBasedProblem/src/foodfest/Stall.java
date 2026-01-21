3e0507a9e660b3799b479453e1dd2abc24e9ba4b
Akash Sharma <i.akashhhh@gmail.com>
2026-01-21 11:39:44 +0530

    [Akash] : Added FoodFest – Sorting Stalls by Customer Footfall

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/foodfest/Stall.java
@@ -0,0 +1,13 @@
+package foodfest;
+
+public class Stall {
+	
+	String stallName;
+	int count;
+	
+	public Stall(String stallName, int count) {
+		this.stallName = stallName;
+		this.count = count;
+	}
+
+}
